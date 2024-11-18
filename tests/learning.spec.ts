9024c815888d9a07c61fb0d9f8c4b511b1ea1815
Restu Pambudi <34370106+sknightz@users.noreply.github.com>
2024-11-19 05:34:35 +0700

    homework restu

--- tests/learning.spec.ts
+++ tests/learning.spec.ts
@@ -1,7 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('Chat GPT', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await page.getByRole('link', { name: 'Get started' }).click();
-  await page.goto('https://playwright.dev/docs/writing-tests');
+test('Open Visual Studio Website', async ({ page }) => {
+  await page.goto('https://www.google.com/');
+  await page.getByLabel('Cari').click();
+  await page.getByLabel('Cari').fill('visual studio');
+  await page.goto('https://www.google.com/search?q=visual+studio&sca_esv=21c38533450dea46&source=hp&ei=6L87Z_H5JeeN4-EPjOT8uA8&iflsig=AL9hbdgAAAAAZzvN-J7pIO3BqTwvSmmQHW6h7_gbdp_C&ved=0ahUKEwjxja2o9-aJAxXnxjgGHQwyH_cQ4dUDCA0&uact=5&oq=visual+studio&gs_lp=Egdnd3Mtd2l6Ig12aXN1YWwgc3R1ZGlvMggQABiABBixAzIIEAAYgAQYsQMyCBAAGIAEGLEDMggQABiABBixAzILEAAYgAQYsQMYgwEyCxAAGIAEGLEDGIMBMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABEjaHFD4B1j3GXABeACQAQCYAXugAYgGqgEEMTIuMbgBA8gBAPgBAZgCDqAC0waoAgrCAhAQABgDGOUCGOoCGIwDGI8BwgIQEC4YAxjlAhjqAhiMAxiPAcICBRAuGIAEmAMKkgcEMTIuMqAH8UM&sclient=gws-wiz');
+  await page.getByRole('link', { name: 'Visual Studio Code - Code' }).click();
+  // await page.getByRole('heading', { name: 'Code faster with AI' }).click();
+  await expect(page.getByRole('heading', { name: 'Code faster with AI' })).toBeVisible();
 });
