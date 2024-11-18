9f91c72d4eb823d7257f22174536fb3f2842135f
Restu Pambudi <34370106+sknightz@users.noreply.github.com>
2024-11-18 21:23:34 +0700

    create script restu

---
+++ tests/learning.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.goto('https://playwright.dev/docs/writing-tests');
+});
