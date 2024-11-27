bd4f99959a2971e00445fb9c38224b074d1d520c
Restu Pambudi <34370106+sknightz@users.noreply.github.com>
2024-11-27 09:21:58 +0700

    fix not global element

--- tests/learning.spec.ts
+++ tests/learning.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('Open Visual Studio Website', async ({ page }) => {
-  await page.goto('https://www.google.com/');
-  await page.getByLabel('Cari').click();
-  await page.getByLabel('Cari').fill('visual studio');
-  await page.goto('https://www.google.com/search?q=visual+studio&sca_esv=21c38533450dea46&source=hp&ei=6L87Z_H5JeeN4-EPjOT8uA8&iflsig=AL9hbdgAAAAAZzvN-J7pIO3BqTwvSmmQHW6h7_gbdp_C&ved=0ahUKEwjxja2o9-aJAxXnxjgGHQwyH_cQ4dUDCA0&uact=5&oq=visual+studio&gs_lp=Egdnd3Mtd2l6Ig12aXN1YWwgc3R1ZGlvMggQABiABBixAzIIEAAYgAQYsQMyCBAAGIAEGLEDMggQABiABBixAzILEAAYgAQYsQMYgwEyCxAAGIAEGLEDGIMBMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABEjaHFD4B1j3GXABeACQAQCYAXugAYgGqgEEMTIuMbgBA8gBAPgBAZgCDqAC0waoAgrCAhAQABgDGOUCGOoCGIwDGI8BwgIQEC4YAxjlAhjqAhiMAxiPAcICBRAuGIAEmAMKkgcEMTIuMqAH8UM&sclient=gws-wiz');
+  await page.goto('https://www.google.com/?hl=en');
+  await page.getByLabel('Search', { exact: true }).click();
+  await page.getByLabel('Search', { exact: true }).fill('visual studio code');
+  await page.goto('https://www.google.com/search?q=visual+studio&sca_esv=92c281187b2f1d14&hl=en&source=hp&ei=_oBGZ4OWOLGV4-EP2_TcwQc&iflsig=AL9hbdgAAAAAZ0aPDl7bylQhwxNZonRik2aTVdzjCzOX&ved=0ahUKEwiDjcfuuPuJAxWxyjgGHVs6N3gQ4dUDCA4&uact=5&oq=visual+studio&gs_lp=Egdnd3Mtd2l6Ig12aXN1YWwgc3R1ZGlvMgcQABiABBgTMgcQABiABBgTMgcQABiABBgTMgcQABiABBgTMgcQABiABBgTMgcQABiABBgTMgcQABiABBgTMgcQABiABBgTMgcQABiABBgTMgcQABiABBgTSJ00UK4ZWJIucAF4AJABAJgBQ6ABigaqAQIxM7gBA8gBAPgBAZgCDqACzAaoAgrCAgoQABgDGOoCGI8BwgIKEC4YAxjqAhiPAZgDC5IHAjE0oAeyQg&sclient=gws-wiz');
   await page.getByRole('link', { name: 'Visual Studio Code - Code' }).click();
   // await page.getByRole('heading', { name: 'Code faster with AI' }).click();
   await expect(page.getByRole('heading', { name: 'Code faster with AI' })).toBeVisible();
