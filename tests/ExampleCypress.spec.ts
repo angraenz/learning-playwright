326bba3f12b4735506ffc8f74937668da9287b16
Restu Pambudi <34370106+sknightz@users.noreply.github.com>
2024-11-29 05:55:04 +0700

    faker homework

--- tests/ExampleCypress.spec.ts
+++ tests/ExampleCypress.spec.ts
@@ -1,18 +1,29 @@
 import { test, expect } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 
+function generateCouponCode() {
+    const prefix = "COUPON"; // Optional prefix
+    const randomCode = faker.string.alphanumeric(10).toUpperCase(); // Generates a 10-character alphanumeric code
+
+    return `${prefix}-${randomCode}`;
+}
+
 test('test', async ({ page }) => {
-    const fakeEmail = faker.internet.email();    
+    const fakeEmail = faker.internet.email(); 
+    const fakePassword = faker.internet.password();
+    const fakeName = faker.internet.displayName();   
+    const fakeDescription = faker.lorem.paragraph();
+    const couponCode = generateCouponCode();
     
     await page.goto('https://example.cypress.io/commands/actions');
     await page.getByPlaceholder('Email').click();
     await page.getByPlaceholder('Email').fill(fakeEmail);
     await page.getByPlaceholder('Password').click();
-    await page.getByPlaceholder('Password').fill('123');
+    await page.getByPlaceholder('Password').fill(fakePassword);
     await page.getByPlaceholder('Enter your name').click();
-    await page.getByPlaceholder('Enter your name').fill('test');
+    await page.getByPlaceholder('Enter your name').fill(fakeName);
     await page.getByLabel('Describe:').click();
-    await page.getByLabel('Describe:').fill('this is a test');
+    await page.getByLabel('Describe:').fill(fakeDescription);
     await page.getByLabel('Coupon Code').click();
-    await page.getByLabel('Coupon Code').fill('a123');
+    await page.getByLabel('Coupon Code').fill(couponCode);
 });
